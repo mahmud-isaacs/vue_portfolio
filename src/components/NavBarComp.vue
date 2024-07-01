69a364e07dcdc953657fdd888e10ce04add2680e
mahmud-isaacs <mahmud.isaacs03@gmail.com>
2024-07-01 16:28:54 +0200

    added nav and footer

---
+++ src/components/NavBarComp.vue
@@ -0,0 +1,61 @@
+<template>
+    <nav class="navbar navbar-expand-lg custom-bg-green">
+      <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <a class="navbar-brand" href="/">
+            <img class="imageLogo" src="https://mahmud-isaacs.github.io/portfolio_hostedImages/images/MI%20logo.jpg" alt="Logo">
+          </a>
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/about">About Me</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/resume">Resume</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/projects">Projects</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/testimonials">Testimonials</router-link>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/contact">Contact Us</a>
+            </li> 
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </template>
+  
+  <script>
+ 
+  </script>
+  
+  <style scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap');
+  .custom-bg-green {
+    background-color: #C4C5BA; 
+  }
+  
+  .nav-link {
+    color: #595f39;
+    font-family: "Archivo Black", sans-serif;
+    font-size: 1,5rem;
+    margin-left: 8rem;
+    transition: color 0.3s ease;
+  }
+  
+  .nav-link:hover {
+    color: #ffffff; 
+  }
+  
+  .imageLogo {
+    width: 40px;
+  }
+  </style>
