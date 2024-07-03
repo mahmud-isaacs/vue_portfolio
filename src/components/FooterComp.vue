acdb6385c83059dfa520e0acec144602a2e57667
mahmud-isaacs <mahmud.isaacs03@gmail.com>
2024-07-03 15:30:45 +0200

    fixed footer

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,38 +1,53 @@
 <template>
-    <footer class="fixed-footer">
-      <div class="footer-content">
-        <div style="justify-content: center;">
-
-        </div>
+  <footer class="fixed-footer">
+    <div class="footer-content">
+      <div style="justify-content: center;">
+        <h6> &copy; Designed and Developed by <b>Mahmud Isaacs</b>|
+        <span id="current-year"></span>  |All rights reserved</h6>
       </div>
-    </footer>
-  </template>
-  
-  <script>
+    </div>
+  </footer>
+</template>
 
-  </script>
-  
-  <style scoped>
-  .fixed-footer {
+<script>
+export default {
+  mounted() {
+    const currentYear = new Date().getFullYear();
+    document.getElementById('current-year').textContent = currentYear;
+  }
+}
+</script>
 
-    bottom: 0;
-    width: 100%;
-    background-color: #C4C5BA; 
-    text-align: center;
-    padding: 10px 0;
-    box-shadow: 0 -2px 5px rgba(0, 0, 0, 0.1);
-    height: 50px; 
-  }
-  
-  .footer-content a img {
-    width: 50px;
-    height: auto;
-  }
-  
-  .footer-content {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-  }
-  </style>
+<style scoped>
+.fixed-footer {
+  bottom: 0;
+  width: 100%;
+  background-color: #C4C5BA; 
+  text-align: center;
+  padding: 10px 0;
+  box-shadow: 0 -2px 5px rgba(0, 0, 0, 0.1);
+  height: 50px; 
+}
+
+.footer-content a img {
+  width: 50px;
+  height: auto;
+}
+
+.footer-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+#current-year {
+  font-size: 1.2rem;
+  color: #595f39;
+}
+
+h6 {
+  font-size: 1rem;
+  color: #595f39;
+}
+</style>
